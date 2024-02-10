548f0c47ccdeab9c75939ca75d1f4ba1ef2ba277
Zongrui Ma <theoneuma9@gmail.com>
2024-02-10 12:47:32 -0500

    move redis action to server action

--- app/api/access/route.ts
+++ app/api/access/route.ts
@@ -1,22 +1,14 @@
 import { headers } from 'next/headers'
 import { getLogger } from '@/logger/log-util'
 import { NextResponse } from 'next/server';
-import { createClient } from 'redis';
+import { updateUserInfo } from '@/server/getUser';
+
 
 
 const USER_EMAIL_ADDRESS_HEADER = 'Cf-Access-Authenticated-User-Email'
 const CF_JWT_HEADER = 'cf-access-jwt-assertion'
 const logger = getLogger("Chat Server Backend");
 
-const redisClient = await createClient({
-    password: 'XnchlcTBWoleK3oMQ2On6CsLRthGNWLr',
-    socket: {
-        host: 'redis-15912.c323.us-east-1-2.ec2.cloud.redislabs.com',
-        port: 15912
-    }
-});
-
-logger.info('const area excuted')
 
 
 export async function GET() {
@@ -27,17 +19,7 @@ export async function GET() {
     logger.info(userEmail)
     logger.info(jwt);
 
-    const redisEmail = await redisClient.get(USER_EMAIL_ADDRESS_HEADER)
-    const redisJWT = await redisClient.get(CF_JWT_HEADER)
-
-    if (redisEmail !== userEmail || redisJWT !== jwt) {
-        logger.info('redis update')
-        await redisClient.set(USER_EMAIL_ADDRESS_HEADER, userEmail || '')
-        await redisClient.set(CF_JWT_HEADER, jwt || '')
-    } else {
-        logger.info('redis up to date')
-
-    }
+    updateUserInfo(userEmail, jwt)
 
     const response = {
         userEmail,
