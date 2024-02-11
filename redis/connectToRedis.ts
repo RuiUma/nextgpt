3999451db5ae7f161bcdf7ba2d1aaae50cef773a
Zongrui Ma <theoneuma9@gmail.com>
2024-02-11 15:27:14 -0500

    update redis info

--- redis/connectToRedis.ts
+++ redis/connectToRedis.ts
@@ -5,7 +5,11 @@ let redisClient: RedisClientType | null = null;
 const connectToRedis = async (): Promise<RedisClientType> => {
     if (!redisClient) {
         redisClient = createClient({
-            url: 'redis://localhost:6379' // Specify your Redis server URI here
+            password: 'XnchlcTBWoleK3oMQ2On6CsLRthGNWLr',
+            socket: {
+                host: 'redis-15912.c323.us-east-1-2.ec2.cloud.redislabs.com',
+                port: 15912
+            }
         });
 
         redisClient.on('error', (err) => console.error('Redis Client Error', err));
